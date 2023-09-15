c156f71233c367e081f70db233101593e35679e7
Jung-Uiseok <tnekwjd3359@naver.com>
2023-09-15 13:07:41 +0900

    onkey 'w','a','s','d' and fix worng word

--- control_turtle.py
+++ control_turtle.py
@@ -1,18 +1,23 @@
 import turtle
 
 def front_move():
-    turtle.setheadin(90)
+    turtle.setheading(90)
     turtle.forward(50)
 
 def back_move():
-    turtle.setheadin(-90)
+    turtle.setheading(-90)
     turtle.forward(50)
 
 def left_move():
-    turtle.setheadin(180)
+    turtle.setheading(180)
     turtle.forward(50)
     
 def right_move():
-    turtle.setheadin(0)
+    turtle.setheading(0)
     turtle.forward(50)
     
+turtle.onkey(front_move, 'w')
+turtle.onkey(back_move, 's')
+turtle.onkey(left_move, 'a')
+turtle.onkey(right_move, 'd')
+turtle.listen()
